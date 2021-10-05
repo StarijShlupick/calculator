f3efbd395cef167a962853186a0e24ae79821c77
Maksim Burakou <maxashtet@gmail.com>
2021-10-05 19:31:41 +0300

    fix: dev server

--- webpack.dev.js
+++ webpack.dev.js
@@ -4,8 +4,7 @@ const common = require('./webpack.common.js')
 module.exports = merge(common, {
   mode: 'development',
   devServer: {
-    host: '0.0.0.0',
-    port: 3030,
+    open: true,
     hot: true,
     inline: true,
     historyApiFallback: true,
